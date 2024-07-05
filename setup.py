40dfade2b5340c98878759fe0e06e3608a5c4f9d
zengqiu <zengqiu@qq.com>
2024-07-05 14:23:48 +0800

    Release version 0.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='django-celery-beat-rest',
-    version='0.0.3',
+    version='0.0.4',
     license='MIT',
     author='zengqiu',
     author_email='zengqiu@qq.com',
