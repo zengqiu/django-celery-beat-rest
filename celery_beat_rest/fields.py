61efc1d8adc5553f0b2a65021f734d410c1b76fa
zengqiu <zengqiu@qq.com>
2024-07-10 16:22:15 +0800

    Enhance availability

--- celery_beat_rest/fields.py
+++ celery_beat_rest/fields.py
@@ -7,4 +7,4 @@ class TimeZoneSerializerChoiceField(TimeZoneSerializerField, ChoiceField):
     def __init__(self, **kwargs):
         self.use_pytz = kwargs.pop('use_pytz', None)
         self.tz_backend = get_tz_backend(use_pytz=self.use_pytz)
-        super().__init__([(tz, tz) for tz in self.tz_backend.base_tzstrs], **kwargs)
+        super().__init__(choices=[(tz, tz) for tz in self.tz_backend.base_tzstrs], **kwargs)
